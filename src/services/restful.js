7d2cdcc006cb8b9e69fd0294fb07db39ac3ecb09
Ale R Buteler <abuteler@gmail.com>
2018-09-03 17:08:25 -0300

    more work.

--- src/services/restful.js
+++ src/services/restful.js
@@ -1,11 +1,7 @@
 import axios from 'axios';
 
 function restAPI(url, method, params) {
-    axios[method](
-            url,
-            { 'Access-Control-Allow-Origin': '*' },
-            { params },
-        ).then(response => {
+    axios[method](url, params).then(response => {
             console.log('in then');
             // JSON responses are automatically parsed.
             return response.data;
@@ -21,6 +17,7 @@ function restAPI(url, method, params) {
                 // The request was made but no response was received
                 // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
                 // http.ClientRequest in node.js
+                console.log('error.request');
                 console.log(error.request);
             } else {
                 // Something happened in setting up the request that triggered an Error
@@ -34,11 +31,11 @@ function getCB() {
         .toString(36)
         .substring(7);
 }
-
+const apiHostUrl = 'http://192.168.28.68:8080';
 export default {
     getAnimal(id) {
         return restAPI(
-            'http://192.168.28.68:8080/entities/animals',
+            `${apiHostUrl}/entities/animals`,
             'get',
             { id: id, cb: getCB() },
         );
@@ -47,7 +44,7 @@ export default {
     getAllAnimalsList() {
         console.log('in getAllAnimalsList');
         return restAPI(
-            'http://192.168.28.68:8080/entities/animals',
+            `${apiHostUrl}/entities/animals`,
             'get',
             { cb: getCB() },
         );
